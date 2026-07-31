c440b2795b05c44fa4044ac4ac1f32a883648750
Karan Rahani <karanrahani6@gmail.com>
2026-07-31 17:30:02 +0530

    Add Image and HyperLink Demo

---
+++ WebApplication2/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication2._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <p>
+        <asp:Image ID="Image1" runat="server" />
+        <br />
+    </p>
+    <p>
+        <asp:HyperLink ID="HyperLink1" runat="server">HyperLink</asp:HyperLink>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </p>
+    <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    </p>
+
+    </asp:Content>
---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Image1.ImageUrl = "/images/BOND&BOOK.jpeg";
+            Image1.Width = 300;
+            Image1.Height = 200;
+            Image1.AlternateText = "BOND";
+
+            HyperLink1.Text = "James Bond Official Website";
+            HyperLink1.NavigateUrl = "https://www.jamesbond.com";
+            HyperLink1.Target = "_blank";
+        }
+    }
+}
